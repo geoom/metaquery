d088040377d5acac60732eeef28d5ff6c2d3b8f4
George Mejia <hi.geoom@gmail.com>
2023-07-13 10:22:36 -0500

    add port

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@ module.exports = {
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT
 }
