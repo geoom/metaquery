a9a1927d8ed0a38a89c9a0fbbd87f231cc24aaa0
George Mejia <hi.geoom@gmail.com>
2023-07-13 03:28:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ require('dotenv').config()
 const debug = require('debug')('app')
 const { useConn, exec } = require('./connector')
 
+
+function jsonToCsv(items) {
+  const header = Object.keys(items[0])
+
+  const headerString = header.join(',')
+  debug("headerString ", headerString)
+
+  // handle null or undefined values here
+  const replacer = (key, value) => value ?? ''
+
+  const rowItems = items.map((row) =>
+    header
+      .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+      .join(',')
+  )
+
+  // join header and body, and break into separate lines
+  const csv = [headerString, ...rowItems].join('\r\n')
+
+  return csv
+}
+
+
 async function startApp () {
   debug('starting metaquery ...')
 
@@ -25,12 +48,14 @@ async function startApp () {
         await exec(dbConn, sqlStatement)
       const jsonResult = JSON.parse(JSON.stringify(myresult))
       
-      if (myresult.length > 0) allMyResults.push([database, jsonResult])
+      if (myresult.length > 0) allMyResults.push(jsonResult)
 
       debug(`progress: ${(index+1)/databases.length*100}%`)
     }
 
-    debug("Result size: ", allMyResults.length)
+    const csv = jsonToCsv(allMyResults.flat(1))
+
+    debug("CSV result \n", csv)
   })
 }
 
