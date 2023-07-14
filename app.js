f5c0337a07bd35970bf32ec940106c84776a9923
George Mejia <hi.geoom@gmail.com>
2023-07-14 00:06:02 -0500

    add file creation

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
-const debug = require('debug')('app')
 const { useConn, exec } = require('./connector')
 
+var fs = require('fs')
+const debug = require('debug')('.')
 
 function jsonToCsv(items) {
   const header = Object.keys(items[0])
 
   const headerString = header.join(',')
-  debug("headerString ", headerString)
 
   // handle null or undefined values here
   const replacer = (key, value) => value ?? ''
@@ -24,39 +24,55 @@ function jsonToCsv(items) {
   return csv
 }
 
+const DBNAME_FILTER_REGEX = /.*/i
+const SQL_STATEMENT = `
+  SELECT DATABASE() as tenant;
+`
 
 async function startApp () {
-  debug('starting metaquery ...')
-
+  console.time('Execution time')
 
   await useConn(async function (dbConn) {
+    debug('METAQUERY STARTS ****\n')
 
     const results = await exec(dbConn, 'SHOW databases')
-
-    databases = results
+    const databases = results
       .map( result => result['Database'])
-
-    debug('databases count: ', databases.length)
-
-    const sqlStatement = `SQL`
-
+      .filter( dbName => DBNAME_FILTER_REGEX.test(dbName))
+    
     const allMyResults = []
     for ([index, database] of databases.entries()) {
-      debug(`USE ${database} ...`)
+      debug(`USING ${database} ...`)
+
       await exec(dbConn, `USE ${database}`)
+
       const myresult = 
-        await exec(dbConn, sqlStatement)
+        await exec(dbConn, SQL_STATEMENT)
       const jsonResult = JSON.parse(JSON.stringify(myresult))
       
       if (myresult.length > 0) allMyResults.push(jsonResult)
 
-      debug(`progress: ${(index+1)/databases.length*100}%`)
+      const progress = parseInt((index+1)/databases.length*100)
+      debug(`progress ----------------------------> ${progress}%`)
     }
 
-    const csv = jsonToCsv(allMyResults.flat(1))
+    debug('*** METAQUERY FINISHES\n')
 
-    debug("CSV result \n", csv)
+    debug('Databases affected size: ', allMyResults.length)
+
+    if(allMyResults.length==0){
+      debug("RESULTS: no data :(\n")
+    } else {
+      const csv = jsonToCsv(allMyResults.flat(1))
+
+      fs.appendFile('results.csv', csv, function (err) {
+        if (err) throw err;
+        console.log('RESULTS: results.csv created!\n');
+      })
+    }
   })
+
+  console.timeEnd('Execution time')
 }
 
 module.exports = {
